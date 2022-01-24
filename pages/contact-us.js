94aef56a422b47fed190df2e2f453e28e7cc1592
samin labbaf <saminlabbaf1994@gmail.com>
2022-01-24 15:46:51 +0330

    add user bar and followers section & socials

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -5,13 +5,15 @@ import BronzePlan from 'assets/images/contact/bronze-plan.svg'
 import SilverPlan from 'assets/images/contact/silver-plan.svg'
 import GoldPlan from 'assets/images/contact/gold-plan.svg'
 import styles from 'styles/pages/ContactUs.module.scss'
-import Card from "components/common/card";
-import Accordion from "components/common/accordion";
-import Categories from "components/homepage/categories";
 import Button from "components/common/button";
 import {useRouter} from "next/router";
 import {useUser} from "hooks/useUser";
 import {useEffect} from "react";
+import Instagram from "assets/svg/social-media/instagram-greeen-circle.svg";
+import Twitter from "assets/svg/social-media/twitter-green-circle.svg";
+import Facebook from "assets/svg/social-media/facebook-green-circle.svg";
+import Linkedin from "assets/svg/social-media/linkedin-greeen-circle.svg";
+import Dots from "assets/svg/common/dots.svg";
 import cookie from "cookie";
 
 export default function Index() {
@@ -103,18 +105,53 @@ export default function Index() {
                                     <li>{feature}</li>
                                 )))}
                             </ul>
-                            {/* <div className={`${styles.buttonContainer} container`}> */}
-                                <Button classes={styles.addContentButton}>
-                                    <Link href='/'>
-                                        <a>
-                                            <span>ملحق شوید</span>
-                                        </a>
-                                    </Link>
-                                </Button>
-                            {/* </div> */}
+                            <Button classes={styles.addContentButton}>
+                                <Link href='/'>
+                                    <a>
+                                        <span>ملحق شوید</span>
+                                    </a>
+                                </Link>
+                            </Button>
                         </div>
                     )))}
                 </div>
+
+                <div className={styles.followersSection}>
+                    {user ? 
+                    <span>{user.followersCount} نفر دنبال کننده</span>
+                    :null
+                    }
+
+                    <div className={styles.social}>
+                        <a href='https://google.com'>
+                            <Image src={Instagram} alt=""/>
+                        </a>
+                        <a href='https://google.com'>
+                            <Image src={Twitter} alt=""/>
+                        </a>
+                        <a href='https://google.com'>
+                            <Image src={Facebook} alt=""/>
+                        </a>
+                        <a href='https://google.com'>
+                            <Image src={Linkedin} alt=""/>
+                        </a>
+                    </div>
+                </div>
+
+                {user ?
+                <div className={styles.bar}>
+                    <div className={styles.user}>
+                        <div className={styles.barImage}>
+                            <Image src={MockAvatar}/>
+                        </div>
+                        <span>{user.username}</span>
+                    </div>
+                    <a href='https://google.com'>
+                        <Image src={Dots} alt=""/>
+                    </a>
+                </div>
+                :null
+                }
         
             </div>
         </div>

729f23ea61b28a94d11e03752e066fab515a3c1d
samin labbaf <saminlabbaf1994@gmail.com>
2022-01-27 19:41:42 +0330

    add using-wallet step to purchase page

--- components/profile/contacts/purchaseCard.js
+++ components/profile/contacts/purchaseCard.js
@@ -2,21 +2,77 @@ import styles from 'styles/components/profile/contacts/PurchaseCard.module.scss'
 import Button from "components/common/button";
 import Image from 'next/image';
 import Sadad from 'assets/images/contact/sadad.svg';
+import ChevronRightLight from 'assets/svg/common/chevron-right-light.svg'
 import Cookies from 'js-cookie';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
-export default function PurchaseCard ({title}) {
+export default function PurchaseCard ({balance, paymentType, title}) {
 
     const router = useRouter()
+    const [step, setStep] = useState('default')
+    const [headerText, setHeaderText] = useState('')
+
+    const payWithWallet = () => {
+        setStep('useWallet')
+    }
+
+    const onChargeWallet = () => {
+        setStep('chargeWallet')
+    }
+
+    const setHeader = () => {
+        let text = ''
+        switch(step) {
+            case 'default':
+                text = 'خرید محتوا'
+                break
+            case 'useWallet':
+                if (balance === 0) {
+                    text = 'موجودی کافی نمی باشد'
+                }
+                break
+            case 'chargeWallet': 
+                text = 'افزایش موجودی'
+                break
+        }
+        setHeaderText(text)
+    }
+
+    const onGoBack = () => {
+        switch(step) {
+            case 'useWallet': 
+                setStep('default')
+                break
+        }
+    }
+
+    useEffect(() => {
+        setHeader()
+    },[step])
 
     return (
         <div className={styles.purchaseCardContainer}>
            <div className={styles.purchaseCard}>
                <div className={styles.purchaseHeader}>
-                   خرید محتوا
+                   {headerText}
                </div>
+               {step !== 'default' ? 
+                <div className={styles.stepBack} onClick={() => onGoBack()}>
+                    <span className={styles.iconContainer}>
+                        <Image src={ChevronRightLight}/>
+                    </span>
+                        <span>
+                        بازگشت به مرحله قبل
+                    </span>
+                </div>
+                :null
+                }
                <div className={styles.title}>
-                    {title}
+                    {step === 'default' ? 
+                    `${title}`
+                    :`${paymentType} هزار تومان بابت خرید محتوا`
+                    }
                </div>
                <div className={styles.cardSubtitle}>
                    <div>
@@ -29,14 +85,25 @@ export default function PurchaseCard ({title}) {
                classes={styles.payButton}
                 >
                     <a>
-                        <span>پرداخت</span>
+                        <span>
+                            {step === 'default' ?
+                            'پرداخت':
+                            'شارژ کیف پول'
+                            }
+                        </span>
                     </a>
                 </Button>
                 <Button variant='outline'
                classes={styles.payButton}
+               onClick={() => payWithWallet()}
                 >
                     <a>
-                        <span>پرداخت با کیف پول</span>
+                        <span>
+                            {step === 'default' ?
+                            'پرداخت با کیف پول':
+                            'پرداخت از درگاه'
+                            }
+                        </span>
                     </a>
                 </Button>
                 <a className={styles.cancelPay}>انصراف</a>
--- pages/contact-us/purchase.js
+++ pages/contact-us/purchase.js
@@ -37,7 +37,7 @@ export default function Purchase () {
             {paymentType === 'اشتراک طلایی' || paymentType === 'اشتراک نقره ای' || paymentType === 'اشتراک برنزی' ? 
             <MembershipPlans />
             :
-            <PurchaseCard title={title}/>
+            <PurchaseCard balance={0} paymentType={paymentType} title={title}/>
             }
         </div>
     )

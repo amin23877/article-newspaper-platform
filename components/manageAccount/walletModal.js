61f1d66b0cd6732657bc90022459b9264f55f920
samin labbaf <saminlabbaf1994@gmail.com>
2022-02-07 19:53:28 +0330

    activate wallet step 2

--- components/manageAccount/walletModal.js
+++ components/manageAccount/walletModal.js
@@ -1,23 +1,72 @@
 import Button from "components/common/button"
 import { useState } from "react"
 import Sadad from 'assets/images/contact/sadad.svg';
+import ChevronRightLight from 'assets/svg/common/chevron-right-light.svg';
 import cardStyles from 'styles/components/profile/contacts/PurchaseCard.module.scss';
-import styles from 'styles/components/manageAccount/ActivateWallet.module.scss'
+import styles from 'styles/components/manageAccount/ActivateWallet.module.scss';
+import Modal from '@mui/material/Modal';
 import Image from "next/image"
 import Link from "next/link";
+import dynamic from 'next/dynamic';
+import { useCountdown } from "hooks/useCountdown";
+import { childNodes } from "dom-helpers";
+const ReactCodeInput = dynamic(import('react-code-input'));
 
 export default function WalletModal ({phone, ...rest}) {
 
     const [step, setStep] = useState('first')
     const [subtitleText, setSubtitleText] = useState('به منظور احراز هویت شماره همراه برای فعال سازی کیف پول، کد فعال سازی به شماره همراه بالا ارسال خواهد شد.')
     const [buttonText, setButtonText] = useState('قبول شرایط و ادامه')
+    
+    const [open, setOpen] = useState(false); // Activate walet conditions modal
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    const [seconds, formattedTime, setIsActive, reset] = useCountdown({initSeconds: 120})
+
+    const onButtonClick = () => {
+        switch(step) {
+            case 'first': 
+                setStep('second')
+                setSubtitleText('کد فعال سازی ارسال شده با پیامک را وارد کنید.')
+                setButtonText('تایید')
+                setIsActive(true)
+                break
+        }
+    }
+
+    const onGoBack = () => {
+        setStep('first')
+    }
+
+    async function onResendOtp() {
+        if (seconds > 0) return
+        //await onOtpRequest(userData.phone)
+        reset()
+    }
 
     return (
         <div className={cardStyles.purchaseCardContainer}>
            <div className={cardStyles.purchaseCard}>
                <div className={cardStyles.purchaseHeader}>
-                   {step !== 'second' ? 'فعال سازی کیف پول' : 'سوالات متداول کیف پول'}
+                   فعال سازی کیف پول
                </div>
+               {step === 'second' ? 
+                <div className={cardStyles.stepBack} onClick={() => onGoBack()}>
+                    <span className={cardStyles.iconContainer}>
+                        <Image src={ChevronRightLight} alt=''/>
+                    </span>
+                        <span>
+                        بازگشت به مرحله قبل
+                    </span>
+                </div>
+                :
+                null
+                }
+               {step === 'second' ? 
+               <div className={styles.modalText}>{`تایید شماره ${phone}`}</div>
+               :null
+                }
                {step === 'first' ? 
                <div className={styles.title}>
                     {phone}
@@ -27,9 +76,21 @@ export default function WalletModal ({phone, ...rest}) {
                 <div className={styles.cardSubtitle}>
                    {subtitleText}
                </div>
+               {step === 'second' ? 
+               <>
+               <ReactCodeInput type='number' fields={4} className={styles.codeInput}/>
+               <div className={styles.timeout}>
+                    {formattedTime}
+                </div>
+                <div className={styles.sendAgain} onClick={() => {onResendOtp()}}>
+                    ارسال مجدد کد تایید
+                </div>
+               </>
+               :null
+                }
                <Button variant='filled'
                classes={[cardStyles.payButton, styles.button]}
-                // onClick={() => onFilledButton()}
+                onClick={() => onButtonClick()}
                 >
                     <a>
                         <span>
@@ -44,14 +105,67 @@ export default function WalletModal ({phone, ...rest}) {
                 :null
                 }
                 {step === 'first' ? 
-                <div className={styles.conditionBtn}>شرایط استفاده از کیف پول</div>
+                <div className={styles.conditionBtn} onClick={() => handleOpen()}>شرایط استفاده از کیف پول</div>
                 :null
                 }
                 <div className={styles.sadadImage}>
                     <Image src={Sadad} alt='Sadad'/>
                 </div>
            </div>
+
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title2"
+                aria-describedby="modal-modal-description2"
+            >
+                <div className={styles.modalContainer}>
+                    <div className={styles.header}>سوالات متداول کیف پول</div>
+                    <div className={styles.text}>
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+                     چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم
+                     است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود 
+                    ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال 
+                    و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها 
+                    شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی 
+                    و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که
+                     تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد و
+                     زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته
+                     اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+                     لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+                     چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم
+                     است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود 
+                    ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال 
+                    و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها 
+                    شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی 
+                    و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که
+                     تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد و
+                     زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته
+                     اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+                     لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+                     چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم
+                     است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود 
+                    ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال 
+                    و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها 
+                    شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی 
+                    و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که
+                     تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد و
+                     زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته
+                     اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+                     لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+                     چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم
+                     است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود 
+                    ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال 
+                    و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها 
+                    شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی 
+                    و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که
+                     تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد و
+                     زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته
+                     اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+                    </div>
+                </div>
                 
+            </Modal> 
         </div>
     )
 }

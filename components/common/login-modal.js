351739f31f7ddaefa8995185c7bc348dd594002d
amin <amin23877@gmail.com>
2022-02-16 11:49:17 +0330

    next link

--- components/common/login-modal.js
+++ components/common/login-modal.js
@@ -1,36 +1,29 @@
-
-import styles from 'styles/common/LoginModal.module.scss'
+import styles from "styles/common/LoginModal.module.scss";
 import Button from "./button";
-import Modal from '@mui/material/Modal';
+import Modal from "@mui/material/Modal";
+import Link from "next/link";
 
-
-export default function LoginModal({handleClose, open = false}) {
-
-    
-
-    return ( 
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <div className={styles.modalContainer}>
-                    <div className={styles.text}>
-                        برای ادامه، وارد حساب کاربری شوید.
-                    </div>
-                    <Button variant='filled' classes={styles.loginButton}
-                     >
-                        <a href='/login'>
+export default function LoginModal({ handleClose, open = false }) {
+    return (
+        <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+        >
+            <div className={styles.modalContainer}>
+                <div className={styles.text}>برای ادامه، وارد حساب کاربری شوید.</div>
+                <Button variant="filled" classes={styles.loginButton}>
+                    <Link href="/login">
+                        <a>
                             <span>ورود به دیجی نشر</span>
                         </a>
-                    </Button>
-                    <a className={styles.cancel} onClick={handleClose}>
-                        انصراف
-                    </a>
-                </div>
-                
-            </Modal>
-        
-    )
+                    </Link>
+                </Button>
+                <a className={styles.cancel} onClick={handleClose}>
+                    انصراف
+                </a>
+            </div>
+        </Modal>
+    );
 }

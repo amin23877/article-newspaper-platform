99972c8f8d823798347cd6ee1f299c33e0503837
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-05 21:04:02 +0330

    refactor layout component and global layout

--- layouts/default.js
+++ layouts/default.js
@@ -2,12 +2,12 @@ import Navbar from "components/layouts/default/navbar";
 import Footer from "components/layouts/default/footer";
 
 
-export default function Layout({ children , pages }) {
+export default function Layout({ children , pages, footer = true }) {
     return (
-        <>
+        <div style={{height: '100%', display: 'flex', flexDirection: 'column'}}>
             <Navbar pages={pages}/>
-            <main>{children}</main>
-            <Footer />
-        </>
+            <main style={{flexGrow: 1, paddingTop: 64}}>{children}</main>
+            {footer && <Footer/>}
+        </div>
     )
 }

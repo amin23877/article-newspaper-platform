6e375c9270d34b13feee1f3c0c1663c5959d6ddc
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-06 21:17:12 +0330

    rename filterBarPanel to filterPanel

--- components/common/filterBar/filterPanel.js
+++ components/common/filterBar/filterPanel.js
@@ -1,17 +1,18 @@
-import React, {useRef} from 'react';
+import React, {useRef} from "react";
 import Text from "components/common/text";
 import classNames from "classnames";
-import styles from 'styles/common/FilterBarPanel.module.scss';
+import styles from "styles/common/FilterBarPanel.module.scss";
 import {useWhenClickOutside} from "hooks/useWhenClickOutside";
 
-function FilterBarPanel({name, activePanel, filters, selected, onSelect, onClose}) {
+function FilterPanel({name, activePanel, filters, selected, onSelect, onClose}) {
     const ref = useRef();
 
     useWhenClickOutside([ref], onClose);
 
     const detectIsSelected = (filter) => selected.includes(filter);
 
-    const handleClick = (value, isSelected) => onSelect(name, value, isSelected ? 'unselect' : 'select');
+    const handleClick = (value, isSelected) =>
+        onSelect(name, value, isSelected ? "unselect" : "select");
 
     return name === activePanel ? (
         <div ref={ref} className={styles.container}>
@@ -21,18 +22,20 @@ function FilterBarPanel({name, activePanel, filters, selected, onSelect, onClose
                 return (
                     <Text
                         key={index}
-                        size='sm'
-                        color={isSelected ? 'black' : 'gray'}
-                        weight={isSelected ? 'bold' : 'normal'}
-                        className={classNames(styles.filter, {[styles.selected]: isSelected})}
+                        size="sm"
+                        color={isSelected ? "black" : "gray"}
+                        weight={isSelected ? "bold" : "normal"}
+                        className={classNames(styles.filter, {
+                            [styles.selected]: isSelected,
+                        })}
                         onClick={() => handleClick(filter.value, isSelected)}
                     >
                         {filter.name}
                     </Text>
-                )
+                );
             })}
         </div>
     ) : null;
 }
 
-export default FilterBarPanel;
+export default FilterPanel;

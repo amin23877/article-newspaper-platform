f712ba8bb95480b65eb698a30fdbd2164f5b8de4
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-06 21:16:30 +0330

    using useReducer instead useState for handle select filters

--- components/common/filterBar/filterBar.js
+++ components/common/filterBar/filterBar.js
@@ -1,74 +1,86 @@
-import styles from 'styles/common/FilterBar.module.scss'
+import styles from "styles/common/FilterBar.module.scss";
 import Magnifier from "assets/svg/common/magnifier-grey.svg";
 import ChevronDown from "assets/svg/common/chevron-down.svg";
 import Image from "next/image";
-import FilterBarPanel from "components/common/filterBar/filterBarPanel";
-import {useState} from "react";
+import FilterPanel from "components/common/filterBar/filterPanel";
+import {useReducer, useState} from "react";
 
 const filters = {
     access: [
-        {name: 'همه', value: 'all'},
-        {name: 'رایگان', value: 'free'},
-        {name: 'اشتراک VIP', value: 'vip'}
+        {name: "همه", value: "all"},
+        {name: "رایگان", value: "free"},
+        {name: "اشتراک VIP", value: "vip"},
     ],
     type: [
-        {name: 'مقاله', value: 'article'},
-        {name: 'ویدیو', value: 'video'},
-        {name: 'پادکست', value: 'podcast'}
+        {name: "مقاله", value: "article"},
+        {name: "ویدیو", value: "video"},
+        {name: "پادکست", value: "podcast"},
     ],
     label: [
-        {name: 'آموزشی', value: 'educational'},
-        {name: 'هنری', value: 'art'},
-        {name: 'سلامت', value: 'health'},
-    ]
-}
+        {name: "آموزشی", value: "educational"},
+        {name: "هنری", value: "art"},
+        {name: "سلامت", value: "health"},
+    ],
+};
 
-export default function FilterBar(props) {
+const initialState = {
+    access: [],
+    label: [],
+    type: [],
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "select":
+            return {
+                ...state,
+                [action.name]: [...state[action.name], action.payload],
+            };
+
+        case "unselect":
+            return {
+                ...state,
+                [action.name]: state[action.name].filter((item) => item !== action.payload),
+            };
+
+        default:
+            throw new Error("invalid action");
+    }
+};
+
+export default function FilterBar() {
     const [activePanel, setActivePanel] = useState(null);
-    const [selectedFilters, setSelectedFilters] = useState({ access: [], label: [], type: [] })
+    const [selectedFilters, dispatch] = useReducer(reducer, initialState);
 
     const closePanel = () => setActivePanel(null);
 
-    const handleSelect = (name, value, action) => {
-        let updater;
-
-        switch (action) {
-            case 'select':
-                updater = (p) => [...p, value];
-                break;
-            case 'unselect':
-                updater = (p) => p.filter(item => item !== value);
-        }
-
-        setSelectedFilters(p => ({
-            ...p,
-            [name]: updater(p[name])
-        }))
-    }
+    // this function get name and value of filter and action type (selected or unselected) and update state
+    const handleSelect = (name, value, type) => {
+        dispatch({type, name, payload: value});
+    };
 
     return (
         <div className={styles.filterBarContainer}>
             <div className={styles.title}>فیلتر</div>
+
             <div className={styles.optionsContainer}>
-                <div onClick={() => setActivePanel('time')} className={styles.optionItem}>
-                    <div className={styles.text}>
-                        زمان
-                    </div>
+                <div onClick={() => setActivePanel("time")} className={styles.optionItem}>
+                    <div className={styles.text}>زمان</div>
+
                     <div className={styles.icon}>
                         <Image src={ChevronDown} alt=""/>
                     </div>
                 </div>
 
-                <div onClick={() => setActivePanel('access')} className={styles.optionItem}>
-                    <div className={styles.text}>
-                        دسترسی
-                    </div>
+                <div onClick={() => setActivePanel("access")} className={styles.optionItem}>
+                    <div className={styles.text}>دسترسی</div>
+
                     <div className={styles.icon}>
                         <Image src={ChevronDown} alt=""/>
                     </div>
 
-                    <FilterBarPanel
-                        name='access'
+                    <FilterPanel
+                        name="access"
                         activePanel={activePanel}
                         filters={filters.access}
                         onClose={closePanel}
@@ -77,16 +89,15 @@ export default function FilterBar(props) {
                     />
                 </div>
 
-                <div onClick={() => setActivePanel('type')} className={styles.optionItem}>
-                    <div className={styles.text}>
-                        نوع محتوا
-                    </div>
+                <div onClick={() => setActivePanel("type")} className={styles.optionItem}>
+                    <div className={styles.text}>نوع محتوا</div>
+
                     <div className={styles.icon}>
                         <Image src={ChevronDown} alt=""/>
                     </div>
 
-                    <FilterBarPanel
-                        name='type'
+                    <FilterPanel
+                        name="type"
                         activePanel={activePanel}
                         filters={filters.type}
                         onClose={closePanel}
@@ -95,16 +106,15 @@ export default function FilterBar(props) {
                     />
                 </div>
 
-                <div onClick={() => setActivePanel('label')} className={styles.optionItem}>
-                    <div className={styles.text}>
-                        برچسب
-                    </div>
+                <div onClick={() => setActivePanel("label")} className={styles.optionItem}>
+                    <div className={styles.text}>برچسب</div>
+
                     <div className={styles.icon}>
                         <Image src={ChevronDown} alt=""/>
                     </div>
 
-                    <FilterBarPanel
-                        name='label'
+                    <FilterPanel
+                        name="label"
                         activePanel={activePanel}
                         filters={filters.label}
                         onClose={closePanel}
@@ -115,11 +125,12 @@ export default function FilterBar(props) {
             </div>
 
             <div className={styles.searchContainer}>
-                <input type="text" placeholder='جستجو'/>
+                <input type="text" placeholder="جستجو"/>
+
                 <span className={styles.iconContainer}>
                     <Image src={Magnifier} alt=""/>
                 </span>
             </div>
         </div>
-    )
+    );
 }

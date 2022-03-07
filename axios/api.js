56b4f2e9eef4e06f140aa08b83f98442314c6c29
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-08 00:37:03 +0330

    create axios instance

---
+++ axios/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+import {Endpoints} from "utils/endpoints";
+
+export const api = axios.create({
+    baseURL: Endpoints.baseUrl,
+    headers: {
+        Authorization: Cookies.get('accessToken')
+    }
+})

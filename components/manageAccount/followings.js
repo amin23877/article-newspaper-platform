9c263e9c0c9832fd989861dd4a409f329a0d7f1e
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-08 00:36:29 +0330

    connect followers and followings pages to api

--- components/manageAccount/followings.js
+++ components/manageAccount/followings.js
@@ -2,35 +2,30 @@ import React from 'react';
 import Table from "components/common/table/Table";
 import TableRow from "components/common/table/TableRow";
 import TableCell from "components/common/table/tableCell";
-
-import MockAvatar from "assets/images/mock-avatar.png";
+import Text from "components/common/text";
+import {useInteractionsList} from "hooks/manage-account/useInteractionsList";
 
 function Followings() {
+    const {list: followings, count, handleLoadMore} = useInteractionsList('followings');
+
+    if (!followings || followings?.length === 0) return <Text>کسی را دنبال نمی کنید</Text>;
+
     return (
-        <Table headers={['دنبال شونده', 'حمایت', 'پرداختی']}>
-            <TableRow>
-                <TableCell icon={MockAvatar}>اسم</TableCell>
-                <TableCell>اشتراک طلایی</TableCell>
-                <TableCell>100 هزار تومان</TableCell>
-            </TableRow>
-
-            <TableRow>
-                <TableCell icon={MockAvatar}>اسم</TableCell>
-                <TableCell>اشتراک طلایی</TableCell>
-                <TableCell>100 هزار تومان</TableCell>
-            </TableRow>
-
-            <TableRow>
-                <TableCell icon={MockAvatar}>اسم</TableCell>
-                <TableCell>اشتراک طلایی</TableCell>
-                <TableCell>100 هزار تومان</TableCell>
-            </TableRow>
-
-            <TableRow>
-                <TableCell icon={MockAvatar}>اسم</TableCell>
-                <TableCell>اشتراک طلایی</TableCell>
-                <TableCell>100 هزار تومان</TableCell>
-            </TableRow>
+        <Table
+            headers={['دنبال شونده', 'حمایت', 'پرداختی']}
+            maxRowsCount={count}
+            size={followings.length}
+            onLoadMore={handleLoadMore}
+        >
+            <tbody>
+                {followings.map(({targetAccount}) => (
+                    <TableRow key={targetAccount._id}>
+                        <TableCell icon={targetAccount.profilePicture}>{targetAccount.username}</TableCell>
+                        <TableCell>دیتا از سرور نمیاد</TableCell>
+                        <TableCell>دیتا از سرور نمیاد</TableCell>
+                    </TableRow>
+                ))}
+            </tbody>
         </Table>
     );
 }

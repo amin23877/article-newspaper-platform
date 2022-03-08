2640e1ac5d211bacac01d66d9203a9604cf18b14
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-08 20:45:14 +0330

    add prop for pass component to show when list is empty

--- components/common/table/table.js
+++ components/common/table/table.js
@@ -2,20 +2,22 @@ import styles from 'styles/common/Table.module.scss'
 import Button from "components/common/button";
 import React from "react";
 
-export default function Table({children, headers, maxRowsCount, size = 10, onLoadMore}) {
+export default function Table({children, headers, maxRowsCount, size = 10, onLoadMore, emptyComponent}) {
     return (
         <>
             <table className={styles.table}>
                 <thead>
-                <tr className={styles.thead}>
-                    {headers.map((header, index) => (
-                        <th className={styles.cell} key={index}>{header}</th>
-                    ))}
-                </tr>
+                    <tr className={styles.thead}>
+                        {headers.map((header, index) => (
+                            <th className={styles.cell} key={index}>{header}</th>
+                        ))}
+                    </tr>
                 </thead>
 
                 {children}
             </table>
+
+            {children.length === 0 && emptyComponent}
 
             {maxRowsCount && maxRowsCount > size && (
                 <div style={{marginTop: 16, display: 'grid', placeItems: 'center'}}>

02f8225e27762f07087c81f2f566bd3ddb0ae5e3
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-08 21:07:13 +0330

    refactor table and add loading state to it

--- components/common/table/table.js
+++ components/common/table/table.js
@@ -1,25 +1,34 @@
 import styles from 'styles/common/Table.module.scss'
 import Button from "components/common/button";
 import React from "react";
+import {CircularProgress} from "@mui/material";
 
-export default function Table({children, headers, maxRowsCount, size = 10, onLoadMore, emptyComponent}) {
+export default function Table({children, headers, data, maxRowsCount, onLoadMore, emptyComponent}) {
     return (
         <>
             <table className={styles.table}>
                 <thead>
-                    <tr className={styles.thead}>
-                        {headers.map((header, index) => (
-                            <th className={styles.cell} key={index}>{header}</th>
-                        ))}
-                    </tr>
+                <tr className={styles.thead}>
+                    {headers.map((header, index) => (
+                        <th className={styles.cell} key={index}>{header}</th>
+                    ))}
+                </tr>
                 </thead>
 
-                {children}
+                <tbody>
+                {data && children(data)}
+                </tbody>
             </table>
 
-            {children.length === 0 && emptyComponent}
+            {!data &&
+                <div className={styles.loading}>
+                    <CircularProgress color='inherit'/>
+                </div>
+            }
 
-            {maxRowsCount && maxRowsCount > size && (
+            {data?.length === 0 && emptyComponent}
+
+            {maxRowsCount && maxRowsCount > data?.length && (
                 <div style={{marginTop: 16, display: 'grid', placeItems: 'center'}}>
                     <Button onClick={onLoadMore}>
                         موارد بیشتر

614ce169c7b6862a8a125b29f3c949ddce147b72
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-10 20:05:16 +0330

    handle general info forms

--- components/manageAccount/personalInfo/generalInfo.js
+++ components/manageAccount/personalInfo/generalInfo.js
@@ -1,128 +1,184 @@
-import React, { useEffect, useRef, useState } from "react";
-import { useForm } from "react-hook-form";
+import React, { useEffect, useMemo, useState } from "react";
+import { useFieldArray, useForm } from "react-hook-form";
 import FormContainer from "components/manageAccount/personalInfo/formContainer";
-import CustomInput from "components/common/input";
-
-import styles from "styles/components/manageAccount/PersonalInfo.module.scss";
-import Text from "components/common/text";
+import { useUpdateUser } from "hooks/manage-account/useUpdateUser";
 import { actual } from ".";
+import styles from "styles/components/manageAccount/PersonalInfo.module.scss";
 
-const ActualUserFields = [
-    {
-        name: "username",
-        label: "نام نام خانوادگی",
-        placeholder: "",
-    },
-    {
-        name: "personNationalId",
-        label: "کد ملی",
-        placeholder: "کد ملی خود را بدون خط تیره وارد نمایید.",
-    },
-    {
-        name: "msisdn",
-        label: "شماره همراه",
-        placeholder: "",
-    },
-    {
-        name: "email",
-        label: "پست الکترونیک",
-        placeholder: "پست الکترونیکتان را وارد نمایید.",
-    },
-];
-
-const LegalUserFields = [
-    {
-        name: "username",
-        label: "نام کاربری",
-    },
-    {
-        name: "companyName",
-        placeholder: "نام ثبتی خود را وارد نمایید.",
-        label: "نام کامل شرکت",
-    },
-    {
-        name: "companyType",
-        placeholder: "نوع شرکت را وارد نمایید",
-        label: "نوع شرکت",
-    },
-    {
-        name: "companyRegisterNum",
-        placeholder: "شماره ثبت شرکت را وارد نمایی.",
-        label: "شماره ثبت",
-    },
-    {
-        name: "companyNationalId",
-        placeholder: "شناسه ملی خود را وارد نمایید.",
-        label: "شناسه ملی",
-    },
-    {
-        name: "companyEconomicId",
-        placeholder: "کد اقتصادی شرکت را وارد نمایید.",
-        label: "کد اقتصادی",
-    },
-    {
-        name: "msisdn",
-        label: "شماره همراه",
-    },
-    {
-        name: "email",
-        placeholder: "پست الکترونیکتان را وارد نمایید.",
-        label: "پست الکترونیک",
-    },
-];
-
-function GeneralInfo({ publisherType, user }) {
-    // detect fields based on publisher type
-    const [fields, setFields] = useState(
-        publisherType === actual ? ActualUserFields : LegalUserFields
-    );
+function GeneralInfoForm({ title, fields, fieldsName }) {
+    const { status, fetcher } = useUpdateUser();
 
     const {
         register,
+        control,
+        reset,
         handleSubmit,
         formState: { errors },
-        setValue,
-    } = useForm();
+    } = useForm({ defaultValues: { [fieldsName]: fields } });
+
+    const { fields: fieldsArray, replace } = useFieldArray({ control, name: fieldsName });
 
     const submitHandler = (data) => {
-        console.log(data);
+        /* The data that the react-hook-form returns is an array of fields with new values,
+         * but the body that must be sent to the server must be an object.
+         * This phrase does this.*/
+        const validData = data[fieldsName].reduce((prev, current) => {
+            prev[current.name] = current.value;
+            return prev;
+        }, {});
+
+        fetcher(validData);
     };
 
-    // update fields when pulisher type changes
+    // update input values if fields changes
     useEffect(() => {
-        if (publisherType === actual) setFields(ActualUserFields);
-        else setFields(LegalUserFields);
-    }, [publisherType]);
+        replace(fields);
+    }, [fields, replace]);
 
-    // set defalut values for inputs
+    // show alert to user when update proccess completed
     useEffect(() => {
-        if (user) {
-            setValue("username", user.username);
-            setValue("msisdn", user.msisdn);
+        if (status === "success") {
+            alert("اطلاعات با موفقیت ویرایش شد");
+        } else if (status === "error") {
+            alert("ویرایش اطلاعات با مشکل مواجه شد");
+
+            // reset form when if update failed
+            reset();
         }
-    }, [setValue, user]);
+    }, [reset, status]);
 
     return (
-        <FormContainer
-            title={publisherType === actual ? 'اطلاعات شخصی': 'اطلاعات حقوقی'}
-            onSubmit={handleSubmit(submitHandler)}
-        >
-            {fields.map((field) => (
-                <div key={field.name} className={styles.formControl}>
-                    <Text component="label" color="black" weight="bold">
-                        {field.label} :
-                    </Text>
-
-                    <CustomInput
-                        register={register}
+        <FormContainer title={title} onSubmit={handleSubmit(submitHandler)}>
+            {fieldsArray.map((field, index) => (
+                <p className={styles.formControl} key={field.id}>
+                    <label>{field.label}</label>
+                    <input
+                        className={styles.input}
                         placeholder={field.placeholder}
-                        name={field.name}
-                        validation={{ required: "پر کردن این فیلد الزامی است" }}
-                        error={errors[field.name]}
+                        {...register(`${fieldsName}.${index}.value`)}
                     />
-                </div>
+                </p>
             ))}
         </FormContainer>
+    );
+}
+
+/*
+ * two belove component only cumpute fields based on user info
+ * and pass to 'GeneralInfoForm' component for handle form
+ */
+
+function LegalUserGeneralInfoForm({ user }) {
+    const LegalUserFields = useMemo(
+        () => [
+            {
+                name: "username",
+                label: "نام کاربری",
+                value: user.username,
+            },
+            {
+                name: "companyName",
+                placeholder: "نام ثبتی خود را وارد نمایید.",
+                label: "نام کامل شرکت",
+                value: user.companyName,
+            },
+            {
+                name: "companyType",
+                placeholder: "نوع شرکت را وارد نمایید",
+                label: "نوع شرکت",
+                value: user.companyType,
+            },
+            {
+                name: "companyRegisterNum",
+                placeholder: "شماره ثبت شرکت را وارد نمایی.",
+                label: "شماره ثبت",
+                value: user.companyRegisterNum,
+            },
+            {
+                name: "companyNationalId",
+                placeholder: "شناسه ملی خود را وارد نمایید.",
+                label: "شناسه ملی",
+                value: user.companyNationalId,
+            },
+            {
+                name: "companyEconomicId",
+                placeholder: "کد اقتصادی شرکت را وارد نمایید.",
+                label: "کد اقتصادی",
+                value: user.companyEconomicId,
+            },
+            {
+                name: "msisdn",
+                label: "شماره همراه",
+                value: user.msisdn,
+            },
+            {
+                name: "email",
+                placeholder: "پست الکترونیکتان را وارد نمایید.",
+                label: "پست الکترونیک",
+                value: user.email,
+            },
+        ],
+        [user]
+    );
+
+    return (
+        <GeneralInfoForm
+            title="اطلاعات حقوقی"
+            fields={LegalUserFields}
+            fieldsName="legalUserInfoForm"
+        />
+    );
+}
+
+function ActualUserGeneralInfoForm({ user }) {
+    const ActaulUserFields = useMemo(
+        () => [
+            {
+                name: "username",
+                label: "نام نام خانوادگی",
+                placeholder: "",
+                value: user.username,
+            },
+            {
+                name: "personNationalId",
+                label: "کد ملی",
+                placeholder: "کد ملی خود را بدون خط تیره وارد نمایید.",
+                value: user.personNationalId,
+            },
+            {
+                name: "msisdn",
+                label: "شماره همراه",
+                placeholder: "",
+                value: user.msisdn,
+            },
+            {
+                name: "email",
+                label: "پست الکترونیک",
+                placeholder: "پست الکترونیکتان را وارد نمایید.",
+                value: user.email,
+            },
+        ],
+        [user]
+    );
+
+    return (
+        <GeneralInfoForm
+            title="اطلاعات حقیقی"
+            fields={ActaulUserFields}
+            fieldsName="actualUserInfoForm"
+        />
+    );
+}
+
+/*
+ * this component return approprite form based on publisher type
+ */
+
+function GeneralInfo({ publisherType, user }) {
+    return publisherType === actual ? (
+        <ActualUserGeneralInfoForm user={user} />
+    ) : (
+        <LegalUserGeneralInfoForm user={user} />
     );
 }
 

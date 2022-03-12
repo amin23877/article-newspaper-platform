016037b595a31e94930c0ea2ffef7320010f63f9
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-12 12:06:19 +0330

    add error color to text component and indent code

--- components/common/text.js
+++ components/common/text.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React from "react";
 import classNames from "classnames";
 
-import styles from 'styles/common/Text.module.scss'
+import styles from "styles/common/Text.module.scss";
 
-function Text({color = 'gray', size = 'm', weight = 'normal', component: Component = 'p', children, className, ...rest}) {
+function Text({
+    color = "gray",
+    size = "m",
+    weight = "normal",
+    component: Component = "p",
+    children,
+    className,
+    ...rest
+}) {
     const classes = classNames({
         [styles[color]]: color,
         [styles[size]]: size,
         [styles[weight]]: weight,
-        [className] : className
-    })
+        [className]: className,
+    });
 
     return (
-        <Component {...rest} className={classes}>{children}</Component>
+        <Component {...rest} className={classes}>
+            {children}
+        </Component>
     );
 }
 

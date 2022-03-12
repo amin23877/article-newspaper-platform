f536cef0aa9d6ae519874578365bd2b60c69f6a1
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-12 21:19:35 +0330

    return response data

--- hooks/manage-account/useUpdateUser.js
+++ hooks/manage-account/useUpdateUser.js
@@ -4,11 +4,13 @@ import { Endpoints } from "utils/endpoints";
 
 export function useUpdateUser() {
     const [status, setStatus] = useState("idle");
+    const [res, setRes] = useState(null);
 
     const fetcher = useCallback((data) => {
         setStatus("loading");
         api.put(Endpoints.updateUser, data)
-            .then(() => {
+            .then((res) => {
+                setRes(res.data.data);
                 setStatus("success");
             })
             .catch(() => {
@@ -16,5 +18,5 @@ export function useUpdateUser() {
             });
     }, []);
 
-    return { status, fetcher };
+    return { res, status, fetcher };
 }

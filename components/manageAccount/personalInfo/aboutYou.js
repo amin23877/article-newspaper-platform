ae6b2cf010c168967d2def3c0c9e65b8491729b8
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-12 12:11:13 +0330

    handle add about-me

--- components/manageAccount/personalInfo/aboutYou.js
+++ components/manageAccount/personalInfo/aboutYou.js
@@ -1,24 +1,42 @@
 import { useForm } from "react-hook-form";
 import FormContainer from "./formContainer";
+import Text from "components/common/text";
 import Textarea from "components/common/textarea";
+import { useUpdateUser } from "hooks/manage-account/useUpdateUser";
+import { useEffect } from "react";
 
-function AboutYou() {
+function AboutYou({ user }) {
+    const { status, fetcher } = useUpdateUser();
+
     const {
         register,
         handleSubmit,
         formState: { errors },
-    } = useForm();
+    } = useForm({ defaultValues: { aboutMe: user.aboutMe } });
 
     const submitHandler = (data) => {
-        console.log(data);
+        fetcher(data);
     };
+
+    useEffect(() => {
+        if (status === "success") alert("اطلاعات با موفقیت ویرایش شد");
+    }, [status]);
 
     return (
         <FormContainer title="درباره تو" onSubmit={handleSubmit(submitHandler)}>
             <Textarea
+                register={register}
+                name="aboutMe"
                 rows="10"
+                validation={{ required: "پر کردن این فیلد الزامی است" }}
                 placeholder="درباره خود و حوزه محتواهایی که تولید میکنید می توانید برای مخاطب خود بنویسید."
             />
+
+            {errors.aboutMe && (
+                <Text style={{ margin: 0 }} color="red" size="sm">
+                    {errors.aboutMe.message}
+                </Text>
+            )}
         </FormContainer>
     );
 }

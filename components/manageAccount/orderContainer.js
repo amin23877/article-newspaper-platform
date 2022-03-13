6fe821dd291fd4ef675febaabe83a023e06332d0
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-13 21:08:39 +0330

    show data based on server response

--- components/manageAccount/orderContainer.js
+++ components/manageAccount/orderContainer.js
@@ -1,58 +1,73 @@
-import styles from 'styles/components/manageAccount/OrderContainer.module.scss'
-import Image from 'next/image'
-import Button from 'components/common/button'
-import Link from 'next/link'
+import styles from "styles/components/manageAccount/OrderContainer.module.scss";
+import Image from "next/image";
+import Button from "components/common/button";
+import Link from "next/link";
 
-export default function OrderContainer ({key, order}) {
+export default function OrderContainer({ order }) {
     return (
-        <div className={styles.container} key={key}>
+        <div className={styles.container}>
             <div className={styles.right}>
-                {order.type.image !== undefined ? 
-                <>
-                    <div className={styles.rightSide}>
-                        <div className={styles.postImage}>
-                            <Image src={order.type.image} alt={order.type.title}/>
-                        </div>
-                    </div>
+                <div className={styles.rightSide}>
+                    <div className={styles.postImage}></div>
+                </div>
+
+                {order.incomeMethod === "buyPost" && (
                     <div className={`${styles.leftSide} ${styles.postLeftSide}`}>
                         <div className={styles.postUser}>
-                            <div className={styles.smallUser}><Image src={order.userAvatar} alt=''/></div>
+                            <div className={styles.smallUser}>
+                                {/* <Image src={order.userAvatar} alt="" /> */}
+                            </div>
+
+                            <div className={styles.username}>نام کاربری</div>
+                        </div>
+
+                        <div className={styles.title}>عنوان</div>
+
+                        <div className={styles.payment}>
+                            <span>{order.amount} تومان</span>{" "}
+                            {order.paymentMethod === "wallet" ? (
+                                <span>از طریق کیف پول</span>
+                            ) : (
+                                <span>از طریق درگاه بانکی</span>
+                            )}
+                        </div>
+                    </div>
+                )}
+
+                {order.incomeMethod === "subscribe" && (
+                    <>
+                        <div className={styles.rightSide}>
+                            <div className={styles.avatar}>
+                                <Image src={order.userAvatar} alt="" />
+                            </div>
                             <div className={styles.username}>{order.username}</div>
                         </div>
-                        <div className={styles.title}>{order.type.title}</div>
-                        <div className={styles.payment}>{order.payment}</div>
-                    </div>
-                </>
-                : 
-                <>
-                    <div className={styles.rightSide}>
-                        <div className={styles.avatar}>
-                            <Image src={order.userAvatar} alt=''/>
+                        <div className={styles.leftSide}>
+                            <div className={styles.title}>{order.type}</div>
+                            <div className={styles.payment}>{order.payment}</div>
                         </div>
-                        <div className={styles.username}>{order.username}</div>
-                    </div>
-                    <div className={styles.leftSide}>
-                        <div className={styles.title}>{order.type}</div>
-                        <div className={styles.payment}>{order.payment}</div>
-                    </div> 
-                </>
-                }
-                
-                
+                    </>
+                )}
             </div>
+
             <div className={styles.left}>
-                <div className={styles.time}>{order.time}</div>
-                {order.type.title === undefined ? 
-                    order.validUntil === 0 ? 
-                    <Link href={{ pathname: '/user/1/purchase', query: {paymentType: 'اشتراک', title: 'عنوان'}}} passHref>
-                        <Button>
-                            تمدید
-                        </Button>
-                    </Link>
-                    :<div className={styles.expiration}>{`${order.validUntil} روز اعتبار`}</div>
-                :null
-                }
+                <div className={styles.time}>{order.createdAt}</div>
+                {/* {order.type.title === undefined ? (
+                    order.validUntil === 0 ? (
+                        <Link
+                            href={{
+                                pathname: "/user/1/purchase",
+                                query: { paymentType: "اشتراک", title: "عنوان" },
+                            }}
+                            passHref
+                        >
+                            <Button>تمدید</Button>
+                        </Link>
+                    ) : (
+                        <div className={styles.expiration}>{`${order.validUntil} روز اعتبار`}</div>
+                    )
+                ) : null} */}
             </div>
         </div>
-    )
+    );
 }
--- components/manageAccount/orderList.js
+++ components/manageAccount/orderList.js
@@ -1,82 +1,29 @@
-import styles from 'styles/components/manageAccount/OrderList.module.scss'
-import OrderContainer from './orderContainer'
-import SearchIcon from 'assets/svg/common/search.svg'
-import MockUser from 'assets/images/contact/mock-avatar.png'
-import Jamejam from 'assets/images/manage-account/jamejam.png'
-import PostImage from 'assets/images/manage-account/post-image.png'
-import Image from 'next/image'
+import styles from "styles/components/manageAccount/OrderList.module.scss";
+import OrderContainer from "./orderContainer";
+import SearchIcon from "assets/svg/common/search.svg";
+import Image from "next/image";
+import { Spinner } from "components/common/spinner";
+import { useOrders } from "hooks/manage-account/useOrders";
 
-export default function OrderList () {
-
-    const orders = [
-        {
-            username: 'Ali Sahari',
-            userAvatar: MockUser,
-            type: 'اشتراک طلایی 3 ماهه',
-            payment: '16 هزار تومان پرداخت از درگاه',
-            time: '11 ساعت پیش',
-            validUntil: 10
-        },
-        {
-            username: 'Ali Sahari',
-            userAvatar: MockUser,
-            type: {
-                title: 'مطالعات کارشناسی ارشد رنگ',
-                image: PostImage
-            },
-            payment: '10 هزار تومان پرداخت از کیف پول',
-            time: '11 ساعت پیش',
-            validUntil: null
-        },
-        {
-            username: 'جام جم',
-            userAvatar: Jamejam,
-            type: 'اشتراک طلایی 3 ماهه',
-            payment: '16 هزار تومان پرداخت از درگاه',
-            time: 'یک هفته پیش',
-            validUntil: 20
-        },
-        {
-            username: 'Ali Sahari',
-            userAvatar: MockUser,
-            type: 'اشتراک طلایی 3 ماهه',
-            payment: '16 هزار تومان پرداخت از درگاه',
-            time: '11 ساعت پیش',
-            validUntil: 0
-        },
-        {
-            username: 'Ali Sahari',
-            userAvatar: MockUser,
-            type: {
-                title: 'مطالعات کارشناسی ارشد رنگ',
-                image: PostImage
-            },
-            payment: '10 هزار تومان پرداخت از کیف پول',
-            time: '11 ساعت پیش',
-            validUntil: null
-        },
-        {
-            username: 'جام جم',
-            userAvatar: Jamejam,
-            type: 'اشتراک طلایی 3 ماهه',
-            payment: '16 هزار تومان پرداخت از درگاه',
-            time: 'یک هفته پیش',
-            validUntil: 0
-        },
-    ]
+export default function OrderList() {
+    const { orders, status } = useOrders();
+    
     return (
         <div className={styles.container}>
             <div className={styles.searchContainer}>
                 <div className={styles.searchIcon}>
-                    <Image src={SearchIcon} alt=""/>
+                    <Image src={SearchIcon} alt="" />
                 </div>
-                <input className={styles.searchInput} placeholder='جستجو' />
+
+                <input className={styles.searchInput} placeholder="جستجو" />
             </div>
-            {orders.map((order, index) => {
-                return (
-                    <OrderContainer key={index} order={order}/>
-                )
-            })}
+
+            {status === "loading" && <Spinner style={{ padding: "5rem" }} />}
+
+            {status === "success" &&
+                orders.map((order, index) => {
+                    return <OrderContainer key={index} order={order} />;
+                })}
         </div>
-    )
+    );
 }

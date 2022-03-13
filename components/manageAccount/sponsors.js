635096bd4f56c5e5ba16c1d39696fb45422dd8da
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-13 18:54:23 +0330

    rename donors to sponsors

---
+++ components/manageAccount/sponsors.js
@@ -0,0 +1,32 @@
+import Table from "components/common/table";
+import TableCell from "components/common/table/tableCell";
+import TableRow from "components/common/table/tableRow";
+import EmptyList from "./emptyList";
+import { useSponsors } from "hooks/manage-account/useSponsors";
+
+import emptyImage from "assets/images/manage-account/empty-list.svg";
+
+export default function Sponsors() {
+    const { list, handleLoadMore } = useSponsors();
+
+    return (
+        <Table
+            headers={["حامی", "حمایت", "پرداختی"]}
+            data={list}
+            onLoadMore={handleLoadMore}
+            emptyComponent={<EmptyList image={emptyImage} text="شما هیچ حامی ندارید" />}
+        >
+            {(data) =>
+                data.map((item) => (
+                    <TableRow key={item._id}>
+                        <TableCell icon={item.profilePicture}>
+                            {item.user}
+                        </TableCell>
+                        <TableCell>دیتا از سرور نمیاد</TableCell>
+                        <TableCell>{item.amount} تومان</TableCell>
+                    </TableRow>
+                ))
+            }
+        </Table>
+    );
+}

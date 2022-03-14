edb4ee9964a717351fcfa9343909c46d598f37a5
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-14 23:22:40 +0330

    refactor carousel component

--- components/common/carousel/carousel.js
+++ components/common/carousel/carousel.js
@@ -1,22 +1,16 @@
-import Link from 'next/link'
 import Image from "next/image";
 import { useRef } from "react";
 
-import { Swiper, SwiperSlide } from 'swiper/react'
-import SwiperCore, { Navigation } from 'swiper';
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation } from "swiper";
 
-import 'swiper/css';
+import "swiper/css";
 
-import MockNews from 'assets/images/953473320video.png'
-import bestSellerBadge from 'assets/svg/common/best-seller-badge.svg'
-import AudioIcon from 'assets/svg/common/audio-icon.svg'
-import VideoIcon from 'assets/svg/common/video-icon.svg'
-import ChevronLeft from 'assets/svg/common/chevron-left.svg'
-import ChevronRight from 'assets/svg/common/chevron-right.svg'
-import styles from 'styles/common/carousel/Carousel.module.scss'
+import ChevronLeft from "assets/svg/common/chevron-left.svg";
+import ChevronRight from "assets/svg/common/chevron-right.svg";
+import styles from "styles/common/carousel/Carousel.module.scss";
 
-export default function Carousel({ items = [], swiperOptions, radius = true, type = '', image = MockNews }) {
-
+export default function Carousel({ items = [], swiperOptions, renderSlide }) {
     SwiperCore.use([Navigation]);
 
     const prevRef = useRef(null);
@@ -25,9 +19,6 @@ export default function Carousel({ items = [], swiperOptions, radius = true, typ
     return (
         <Swiper
             spaceBetween={24}
-            slidesPerView={3}
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
             navigation={{
                 // Both prevEl & nextEl are null at render so this does not work
                 prevEl: prevRef.current,
@@ -41,62 +32,21 @@ export default function Carousel({ items = [], swiperOptions, radius = true, typ
             }}
             {...swiperOptions}
         >
-            {
-                items.map((item, index) => {
-                    return (
-                        <SwiperSlide key={index}>
-                            {
-                                item.bestSeller ?
-                                    (<div className={styles.bestSellerBadge}>
-                                        <Image src={bestSellerBadge} alt='' />
-                                    </div>) :
-                                    ''
-                            }
-                            <Link href={item.route}>
-                                <a className={
-                                    `${styles.anchor} ${radius ? styles.radius : ''} ${item.contentType === 'podcast' || item.contentType === 'video' ? styles.grayScale : ''}`
-                                }>
-                                    <div className={styles.iconContainer}>
-                                        {
-                                            (() => {
-                                                switch (item.contentType) {
-                                                    case ('podcast'):
-                                                        return (
-                                                            <div className={styles.icon}>
-                                                                <Image src={AudioIcon} alt="" />
-                                                            </div>
-                                                        )
-                                                    case ('video'):
-                                                        return (
-                                                            <div className={styles.icon}>
-                                                                <Image src={VideoIcon} alt="" />
-                                                            </div>
-                                                        )
-                                                }
-                                            })()
-                                        }
-                                        <Image loader={() => item.image} height={type == 'best' ?'300px':(type=='magazine' || type=='article')?'250px':'200px'} width="100%" className={`${styles.imageItem}`} src={item.image} alt="" />
-                                    </div>
-                                    {item.title || item.subTitle ?
-                                        (
-                                            <div className={styles.descriptionContainer}>
-                                                {item.title ? <div className={styles.title}>{item.title}</div> : null}
-                                                {item.subTitle ? <div className={styles.subTitle}>{item.subTitle}</div> : null}
-                                            </div>
-                                        ) :
-                                        ''}
-                                </a>
-                            </Link>
-                        </SwiperSlide>
-                    )
-                })
-            }
+            {items.map((item, index) => {
+                return (
+                    <SwiperSlide key={index}>
+                        {typeof renderSlide === "function" && renderSlide(item)}
+                    </SwiperSlide>
+                );
+            })}
+
             <div className={styles.prevNav} ref={prevRef}>
                 <Image src={ChevronRight} alt="" />
             </div>
+
             <div className={styles.nextNav} ref={nextRef}>
                 <Image src={ChevronLeft} alt="" />
             </div>
         </Swiper>
     );
-};
+}
--- components/common/carousel/carouselContainer.js
+++ components/common/carousel/carouselContainer.js
@@ -1,18 +1,22 @@
 import Carousel from "./carousel";
-import Link from 'next/link';
+import Link from "next/link";
 
-import styles from 'styles/common/carousel/CarouselContainer.module.scss'
+import styles from "styles/common/carousel/CarouselContainer.module.scss";
 
-export default function CarouselContainer ({title, seeAllText = 'مشاهده همه', carouselOptions}) {
+export default function CarouselContainer({ title, seeAllText = "مشاهده همه", carouselOptions }) {
     return (
         <div className={styles.carouselContainer}>
             <div className={styles.carouselHeader}>
                 <div className={styles.title}>{title}</div>
-                <div className={styles.showAllContainer}><Link href='/' as={carouselOptions.page}>{seeAllText}</Link></div>
+                <div className={styles.showAllContainer}>
+                    <Link href="/" as={carouselOptions.page}>
+                        {seeAllText}
+                    </Link>
+                </div>
             </div>
             <div className={styles.sliderContainer}>
                 <Carousel {...carouselOptions} />
             </div>
         </div>
-    )
+    );
 }

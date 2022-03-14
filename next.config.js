2df263608306e90eba774f91323b9c953cf8715f
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-14 23:23:40 +0330

    add hostname

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  reactStrictMode: true,
-  ignoreDuringBuilds: true,
-  swcminify: false,
-  images: {
-    domains: ['http://158.255.74.155:9000/images/:id*'],
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/v1/:path*',
-        destination: 'http://158.255.74.155:4402/v1/:path*'
-      }
-    ]
-  }
-}
+    reactStrictMode: true,
+    ignoreDuringBuilds: true,
+    swcminify: false,
+    images: {
+        domains: ["http://158.255.74.155:9000/images/:id*", "158.255.74.155"],
+    },
+    async rewrites() {
+        return [
+            {
+                source: "/v1/:path*",
+                destination: "http://158.255.74.155:4402/v1/:path*",
+            },
+        ];
+    },
+};

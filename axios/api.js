6b835f920d6044717cf0c0ad9a4553ba99fa0972
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-03-14 23:23:21 +0330

    insert empty string if cookie is not defined

--- axios/api.js
+++ axios/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import Cookies from "js-cookie";
-import {Endpoints} from "utils/endpoints";
+import { Endpoints } from "utils/endpoints";
 
 export const api = axios.create({
     baseURL: Endpoints.baseUrl,
     headers: {
-        Authorization: Cookies.get('accessToken')
-    }
-})
+        Authorization: Cookies.get("accessToken") || "",
+    },
+});

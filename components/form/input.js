0ce2f4c41c376970e18add2534011fda29a95ea4
hassan <hassanebrahimi560@gmail.com>
2022-04-12 18:02:47 +0430

    format code

--- components/form/input.js
+++ components/form/input.js
@@ -1,11 +1,27 @@
-
 import styles from "styles/common/Input.module.scss";
 
-export default function Input({ register, validation, name,type, label, classes, labelClasses, ...rest }) {
-    return (
-        <>
-            {label ? <label className={`${styles.label} ${labelClasses}`}>{label}</label> : ''}
-            <input type={type} className={`${styles.input} ${classes}`} {...register(name, validation)} {...rest} />
-        </>
-    );
+export default function Input({
+  register,
+  validation,
+  name,
+  type,
+  label,
+  classes,
+  labelClasses,
+  ...rest
+}) {
+  return (
+    <>
+      {label && (
+        <label className={`${styles.label} ${labelClasses}`}>{label}</label>
+      )}
+
+      <input
+        type={type}
+        className={`${styles.input} ${classes}`}
+        {...register(name, validation)}
+        {...rest}
+      />
+    </>
+  );
 }

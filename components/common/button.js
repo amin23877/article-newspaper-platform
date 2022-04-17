0d9759e6887bd6e8a66ca525829331dfa10680d8
hassan <hassanebrahimi560@gmail.com>
2022-04-17 19:02:04 +0430

    fix button style bug and format code

--- components/common/button.js
+++ components/common/button.js
@@ -1,15 +1,25 @@
+import styles from "styles/common/Button.module.scss";
+import classNames from "classnames";
 
-import styles from 'styles/common/Button.module.scss'
-import classNames from "classnames"
-
-export default function Button({children, formState, variant = 'filled', classes, disabled, ...rest}) {
-    return (
-        <button className={
-            classNames(styles.button, styles[variant], classes, disabled ? styles.disabled : '')
-        }
-                {...rest}
-        >
-            {children}
-        </button>
-    )
+export default function Button({
+  children,
+  formState,
+  variant = "filled",
+  classes,
+  disabled,
+  ...rest
+}) {
+  return (
+    <button
+      className={classNames(
+        styles.button,
+        styles[variant],
+        classes,
+        disabled ? styles.disabled : ""
+      )}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
 }

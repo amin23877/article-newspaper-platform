e280140b55bfea667700ec46245872f3d9546be3
hassan <hassanebrahimi560@gmail.com>
2022-04-18 14:20:36 +0430

    refactor text component and add new features

---
+++ components/common/typography/text.js
@@ -0,0 +1,30 @@
+import React from "react";
+import classNames from "classnames";
+
+import styles from "styles/common/Text.module.scss";
+
+function Text({
+  color = "gray",
+  size = "m",
+  weight = "normal",
+  align = "right",
+  component: Component = "p",
+  children,
+  className,
+  ...rest
+}) {
+  const classes = classNames(
+    styles[color],
+    styles[size],
+    styles[weight],
+    styles["align-" + align],
+    className
+  );
+  return (
+    <Component {...rest} className={classes}>
+      {children}
+    </Component>
+  );
+}
+
+export default Text;

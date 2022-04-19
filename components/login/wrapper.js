6fe32fca48cb1de619c81edf40b45517dc0c44de
hassan <hassanebrahimi560@gmail.com>
2022-04-20 01:09:06 +0430

    responsive login page

--- components/login/wrapper.js
+++ components/login/wrapper.js
@@ -1,19 +1,28 @@
 import styles from "styles/components/login/Wrapper.module.scss";
+import Text from "components/common/typography/text";
 
-export default function Wrapper({children}) {
-    return (
-        <div className={styles.pageContainer}>
-            <div className={styles.rightCol}>
-                {children}
-            </div>
-            <div className={styles.leftCol}>
-                <div className={styles.bgShadow}/>
-                <div className={styles.circle}/>
-                <div className={styles.textWrapper}>
-                    <div className={styles.title}>Digi Nashr Logo</div>
-                    <div className={styles.description}>دیجی نشر سایت و اپلیکیشن جهت مطالعه و دانلود و خرید نشریات و مجلات ،مقاله و صوتی به صورت قانونی است.</div>
-                </div>
-            </div>
+export default function Wrapper({ children }) {
+  return (
+    <div className={styles.pageContainer}>
+      <div className={styles.rightCol}>{children}</div>
+      <div className={styles.leftCol}>
+        <div className={styles.bgShadow} />
+        <div className={styles.circle} />
+        <div className={styles.textWrapper}>
+          <Text
+            weight="bold"
+            color="white"
+            size="huge"
+            className={styles.title}
+          >
+            Digi Nashr Logo
+          </Text>
+          <Text align="center" color="white" className={styles.description}>
+            دیجی نشر سایت و اپلیکیشن جهت مطالعه و دانلود و خرید نشریات و مجلات
+            ،مقاله و صوتی به صورت قانونی است.
+          </Text>
         </div>
-    )
+      </div>
+    </div>
+  );
 }

966209e3597bd1a723273edf7af4e2fd0f532d22
hassan <hassanebrahimi560@gmail.com>
2022-04-21 12:58:05 +0430

    responsive add content details section

--- components/profile/addContent/wrapper-card.js
+++ components/profile/addContent/wrapper-card.js
@@ -1,30 +1,32 @@
+import Image from "next/image";
 
-import Image from 'next/image'
+import styles from "styles/components/profile/addContent/WrapperCard.module.scss";
 
-import styles from 'styles/components/profile/addContent/WrapperCard.module.scss'
+import InfoIcon from "assets/svg/common/info.svg";
+import Text from "components/common/typography/text";
 
-import InfoIcon from 'assets/svg/common/info.svg'
-
-export default function WrapperCard({children, title, description, className}) {
-
-    return (
-        <div className={`${styles.cardContainer} ${className}`}>
-            <div className={styles.titleContainer}>
-                <div className={styles.title}>{title}:</div>
-                <div className={styles.infoIcon}>
-                    <Image src={InfoIcon} alt=''/>
-                </div>
-            </div>
-            {description ?
-                (
-                    <div className={styles.description}>
-                        {description}
-                    </div>
-                ) : ''
-            }
-            <div className={styles.content}>
-                {children}
-            </div>
+export default function WrapperCard({
+  children,
+  title,
+  description,
+  className,
+}) {
+  return (
+    <div className={`${styles.cardContainer} ${className}`}>
+      <div className={styles.titleContainer}>
+        <Text color="black" size="l" weight="bold" className={styles.title}>
+          {title}:
+        </Text>
+        <div className={styles.infoIcon}>
+          <Image src={InfoIcon} alt="" />
         </div>
-    )
+      </div>
+      {description ? (
+        <Text className={styles.description}>{description}</Text>
+      ) : (
+        ""
+      )}
+      {children}
+    </div>
+  );
 }

2324d16786b897d2109b100fe78807bf366ae597
hassan <hassanebrahimi560@gmail.com>
2022-04-21 13:58:19 +0430

    responsive add content successful modal

--- components/profile/addContent/backDrop.js
+++ components/profile/addContent/backDrop.js
@@ -1,15 +1,20 @@
-import * as React from 'react';
-import Backdrop from '@mui/material/Backdrop';
-import CircularProgress from '@mui/material/CircularProgress';
-import Button from '../../common/button';
-import styles from '../../../styles/components/profile/addContent/steps/Completion.module.scss'
-import Box from "@mui/material/Box"
-import Image from 'next/image';
-import Typography from "@mui/material/Typography"
-import success from 'assets/svg/common/success.svg'
-export default function SimpleBackdrop({ handleAddPost, openDialog, setOpenDialog }) {
-  const handleClose = () => {
+import * as React from "react";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
+import Button from "../../common/button";
+import styles from "../../../styles/components/profile/addContent/steps/Completion.module.scss";
+import Box from "@mui/material/Box";
+import Image from "next/image";
+import Typography from "@mui/material/Typography";
+import success from "assets/svg/common/success.svg";
+import Text from "components/common/typography/text";
 
+export default function SimpleBackdrop({
+  handleAddPost,
+  openDialog,
+  setOpenDialog,
+}) {
+  const handleClose = () => {
     setOpenDialog(false);
     window.location.reload();
   };
@@ -19,31 +24,37 @@ export default function SimpleBackdrop({ handleAddPost, openDialog, setOpenDialo
 
   return (
     <div>
-      <Button onClick={handleAddPost} classes={styles.button}>تایید نهایی</Button>
+      <Button onClick={handleAddPost} classes={styles.button}>
+        تایید نهایی
+      </Button>
       <Backdrop
-        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
         open={openDialog}
         onClick={handleClose}
       >
         <Box
           width="660px"
-          height="392px" background='#FFFFFF'
-          border='4px solid rgba(21, 90, 97, 0.5);'
-          boxSizing='border-box'
-          boxShadow='0px 4px 4px rgba(0, 0, 0, 0.25)'
-          borderRadius='30px 30px 30px 0px'
+          height="392px"
+          background="#FFFFFF"
+          border="4px solid rgba(21, 90, 97, 0.5);"
+          boxSizing="border-box"
+          boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
+          borderRadius="30px 30px 30px 0px"
           display="flex"
-          justifyContent='center'
-          alignItems='center'
-          flexDirection='column'
-          bgcolor='#FFFFFF'>
-          <Box marginBottom='40px'>
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+          margin="1rem"
+          bgcolor="#FFFFFF"
+        >
+          <Box marginBottom="40px">
             <Image src={success} alt="" />
           </Box>
           <Box>
-            <Typography fontSize='24px' fontWeight='700' fontFamily='IRANSans' color='#155A61'>محتوای شما با موفقیت بارگذاری شد </Typography>
+            <Text color="primary" weight="bold" size="xxl">
+              محتوای شما با موفقیت بارگذاری شد{" "}
+            </Text>
           </Box>
-
         </Box>
         {/* <CircularProgress color="inherit" /> */}
       </Backdrop>

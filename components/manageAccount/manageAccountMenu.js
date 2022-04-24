387fb36c834686a03fc6b7ed4197449323de9f7d
hassan <hassanebrahimi560@gmail.com>
2022-04-24 22:03:22 +0430

    create manage account menu drawer

---
+++ components/manageAccount/manageAccountMenu.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useMenuItems } from "hooks/manage-account/useMenuItems";
+import styles from "styles/pages/ManageAccount.module.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { changeActiveMenu } from "redux/manage-account";
+import Drawer from "components/common/drawer";
+import UserInformationSummery from "components/manageAccount/userInformationSummery";
+
+function ManageAccountMenu(user) {
+  const menu = useMenuItems(user);
+
+  const dispatch = useDispatch();
+
+  const activeMenu = useSelector((state) => state.manageAccount.activeMenu);
+
+  const onChangeMenu = (index) => {
+    dispatch(changeActiveMenu(index));
+  };
+
+  return (
+    <Drawer>
+      <UserInformationSummery user={user} />
+
+      <ul className={styles.menuItems}>
+        {menu.map((item, index) => {
+          if (!item.isPublic && !user.isContentProvider) return null;
+
+          return (
+            <li
+              key={index}
+              onClick={() => onChangeMenu(index)}
+              className={activeMenu === index ? styles.activeMenu : styles.menu}
+            >
+              {item.name}
+            </li>
+          );
+        })}
+      </ul>
+    </Drawer>
+  );
+}
+
+export default ManageAccountMenu;

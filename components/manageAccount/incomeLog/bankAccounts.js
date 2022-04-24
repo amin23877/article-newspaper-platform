b50c6ef3c0fd8596a67b9f04c6c0898f35b6bbd1
hassan <hassanebrahimi560@gmail.com>
2022-04-24 22:15:55 +0430

    remove add new account button

--- components/manageAccount/incomeLog/bankAccounts.js
+++ components/manageAccount/incomeLog/bankAccounts.js
@@ -2,48 +2,44 @@ import { useState } from "react";
 import Button from "components/common/button";
 import styles from "styles/components/manageAccount/IncomeLog.module.scss";
 
-export default function BankAccounts({ setAddAccount, banksData }) {
-    const [accountIndex, setAccountIndex] = useState();
+export default function BankAccounts({ banksData }) {
+  const [accountIndex, setAccountIndex] = useState();
 
-    const changeType = (index) => {
-        setAccountIndex(index);
-    };
+  const changeType = (index) => {
+    setAccountIndex(index);
+  };
 
-    return (
-        <div className={`${styles.accounts} ${styles.container}`}>
-            <div className={styles.header}>حساب های متصل</div>
+  return (
+    <div className={`${styles.accounts} ${styles.container}`}>
+      <div className={styles.header}>حساب های متصل</div>
 
-            <div className={styles.description}>
-                اطلاعات حسابی که میخواهید واریز به آن انجام شود را وارد نمایید.
-            </div>
+      <div className={styles.description}>
+        اطلاعات حسابی که میخواهید واریز به آن انجام شود را وارد نمایید.
+      </div>
 
-            <div className={styles.radioButtons}>
-                {banksData.map((account, index) => (
-                    <div key={account._id} className={styles.realLabel}>
-                        <label>
-                            <input
-                                type="radio"
-                                name="type"
-                                value={account.number}
-                                checked={accountIndex === index}
-                                onChange={() => changeType(index)}
-                            />
+      <div className={styles.radioButtons}>
+        {banksData.map((account, index) => (
+          <div key={account._id} className={styles.realLabel}>
+            <label>
+              <input
+                type="radio"
+                name="type"
+                value={account.number}
+                checked={accountIndex === index}
+                onChange={() => changeType(index)}
+              />
 
-                            <span></span>
-                        </label>
-                        <span>{account.title}</span>&nbsp;&nbsp;&nbsp;
-                        <span>{account.number}</span>
-                    </div>
-                ))}
+              <span></span>
+            </label>
+            <span>{account.title}</span>&nbsp;&nbsp;&nbsp;
+            <span>{account.number}</span>
+          </div>
+        ))}
+      </div>
 
-                <div className={styles.addAccount} onClick={() => setAddAccount(true)}>
-                    افزودن حساب جدید
-                </div>
-            </div>
-
-            <div className={styles.buttonContainer}>
-                <Button>تسویه حساب</Button>
-            </div>
-        </div>
-    );
+      <div className={styles.buttonContainer}>
+        <Button>تسویه حساب</Button>
+      </div>
+    </div>
+  );
 }

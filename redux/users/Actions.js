985059f0dca5e9851b1aefad2ad2868cd7a75bc1
hassan <hassanebrahimi560@gmail.com>
2022-04-26 16:33:58 +0430

    add new action for update single field in user state

--- redux/users/Actions.js
+++ redux/users/Actions.js
@@ -1,14 +1,26 @@
 export const SET_USER_INFO = "setUserInfo";
+export const SET_USER_INFO_SECTION = "setUserInfoSection";
 export const SET_TIMEZONES = "setTimezones";
 
 export const setUserInfo = (payload) => async (dispatch) => {
-    try {
-        dispatch({
-            type: SET_USER_INFO,
-            payload: payload,
-        });
-        return;
-    } catch (e) {
-        console.log(e);
-    }
+  try {
+    dispatch({
+      type: SET_USER_INFO,
+      payload: payload,
+    });
+    return;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const setUserInfoSection = (section, value) => async (dispatch) => {
+  try {
+    dispatch({
+      type: SET_USER_INFO_SECTION,
+      payload: { [section]: value },
+    });
+  } catch (e) {
+    console.log(e);
+  }
 };
--- redux/users/Reducer.js
+++ redux/users/Reducer.js
@@ -1,20 +1,26 @@
-import {
-    SET_USER_INFO
-} from "./Actions";
+import { SET_USER_INFO, SET_USER_INFO_SECTION } from "./Actions";
 
 const defaultState = {
-    userInfo: null,
+  userInfo: null,
 };
 
 export default (state = defaultState, action) => {
-    const { type, payload } = action;
-    switch (type) {
-        case SET_USER_INFO:
-            return {
-                ...state,
-                userInfo: payload
-            }
-        default:
-            return state;
-    }
+  const { type, payload } = action;
+  switch (type) {
+    case SET_USER_INFO:
+      return {
+        ...state,
+        userInfo: payload,
+      };
+    case SET_USER_INFO_SECTION:
+      return {
+        ...state,
+        userInfo: {
+          ...state.userInfo,
+          ...payload,
+        },
+      };
+    default:
+      return state;
+  }
 };
